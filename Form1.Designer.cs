c498ffe11c6d83018a6c09e48dc42c2c8b9682d1
violet89 <violet89@VIOLET89-PC>
2016-09-22 18:36:35 +0900

    first commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(128, 83);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(62, 12);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "Test!!!!!!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblTest);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTest;
     }
 }
 
