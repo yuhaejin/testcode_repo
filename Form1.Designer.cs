6e0337bc599dc0ffc87051e718a6cc205fa45984
violet89 <violet89@VIOLET89-PC>
2016-09-23 11:16:44 +0900

    2nd commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.lblTest = new System.Windows.Forms.Label();
+            this.txtTest = new System.Windows.Forms.TextBox();
+            this.lblTest002 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTest
@@ -40,11 +42,29 @@
             this.lblTest.TabIndex = 0;
             this.lblTest.Text = "Test!!!!!!!!";
             // 
+            // txtTest
+            // 
+            this.txtTest.Location = new System.Drawing.Point(142, 180);
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(100, 21);
+            this.txtTest.TabIndex = 1;
+            // 
+            // lblTest002
+            // 
+            this.lblTest002.AutoSize = true;
+            this.lblTest002.Location = new System.Drawing.Point(83, 120);
+            this.lblTest002.Name = "lblTest002";
+            this.lblTest002.Size = new System.Drawing.Size(60, 12);
+            this.lblTest002.TabIndex = 2;
+            this.lblTest002.Text = "Test00002";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblTest002);
+            this.Controls.Add(this.txtTest);
             this.Controls.Add(this.lblTest);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +76,8 @@
         #endregion
 
         private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.Label lblTest002;
     }
 }
 
